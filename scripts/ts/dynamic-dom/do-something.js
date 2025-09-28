d4e38ff34281a63a0aaf7f5592258effb1bd92a0
Emily Huang <emilyhuang405@gmail.com>
2025-09-28 15:53:29 -0500

    areas of study page

--- scripts/ts/dynamic-dom/do-something.js
+++ scripts/ts/dynamic-dom/do-something.js
@@ -1,3 +1,17 @@
 export function doSomething() {
-  console.log("Put some code in here!");
+	console.log("Put some code in here!");
 }
+
+export function showTab(tabId, link) {
+	// Remove active from all tabs and content
+	document.querySelectorAll(".tab-link").forEach(l => l.classList.remove("active"));
+	document.querySelectorAll(".tab-content").forEach(c => c.classList.remove("active"));
+
+	// Activate clicked tab
+	link.classList.add("active");
+
+	// Show correct content
+	document.getElementById(tabId).classList.add("active");
+}
+
+window.showTab = showTab;
